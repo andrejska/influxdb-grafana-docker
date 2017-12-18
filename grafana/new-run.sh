3c667b364f2255a4aca95106010b8dfb216221fb
Andrejs Kalnačs <akalnacs@evolutiongaming.com>
2017-12-18 21:46:35 +0200

    For more convenient use name of data source made same as database name

--- grafana/new-run.sh
+++ grafana/new-run.sh
@@ -4,7 +4,7 @@
 timeout 10 bash -c "until </dev/tcp/localhost/3000; do sleep 1; done"
 
 curl 'http://admin:admin@localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' \
---data-binary '{"name":"influx","type":"influxdb","url":"http://influxdb:8086","access":"proxy","isDefault":true,"database":"js_performance","user":"admin","password":"admin"}'
+--data-binary '{"name":"js_performance","type":"influxdb","url":"http://influxdb:8086","access":"proxy","isDefault":true,"database":"js_performance","user":"admin","password":"admin"}'
 
 pkill grafana-server
 timeout 10 bash -c "while </dev/tcp/localhost/3000; do sleep 1; done"
